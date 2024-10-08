041f194c923105efbbc6ff8ef5504fbae85ee4a4
Zvyagintsev Sergey <zvyagintsevsergey@yandex.ru>
2024-10-08 16:08:20 +0300

    ready HW lesson 6

--- src/main/java/ASTON_HW/Main.java
+++ src/main/java/ASTON_HW/Main.java
@@ -2,6 +2,15 @@ package ASTON_HW;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee [] employees = new Employee[5];
+        employees [0] = new Employee("Иван Смирнов","Аналитик","smirnov@mail.ru", "4951111",12000,30);
+        employees [1] = new Employee("Сергей Петров","Менеджер","petrov@mail.ru", "4951212",20000,32);
+        employees [2] = new Employee("Илья Федоров","Директор","fedorov@mail.ru", "4950000",80000,37);
+        employees [3] = new Employee("Ирина Кит","Инженер","kit@mail.ru", "4955555",40000,35);
+        employees [4] = new Employee("Елена Икс","Секретарь","iks@mail.ru", "4946551",23000,19);
+
+
+
     }
 }
